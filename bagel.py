78640059293e04022d2b5a3d70c077561c14da27
Zach Bunting <zvrbunting@gmail.com>
2024-12-27 12:39:11 -0800

    Working draft.

--- bagel.py
+++ bagel.py
@@ -2,39 +2,48 @@ import random
 
 GUESSES = 10
 
-# get a three-digit number at random
-rand_num = random.randint(100,999)
-print(rand_num)
+while (True):
+    print("I am thinking of a 3-digit number. Try to guess what it is")
+    print("Here are some clues:")
+    print("When I say:     That means:")
+    print("  Pico          One digit is correct but in the wrong position.")
+    print("  Fermi         One digit is correct and in the right position.")
+    print("  Bagels        No digit is correct.")
 
-stringified_num = str(rand_num)
+    num_guesses = 0
+    rand_num = random.randint(100,999)
+    stringified_num = str(rand_num)
 
-# 312
-# 483
+    print("I have thought up a number.")
+    print(f' You have {GUESSES} guesses to get it.')
 
-# initialize variables for correct digits and correct places at 0
-# for each digit,
-# check for direct equality first at same index = increment by 1 if true
-# if falsy, check if that value is in the game's number at all = increment by 1 if true
+    while(True):
+        num_guesses += 1
+        print(f'Guess #{num_guesses}')
+        s = input("Enter your guess for the number here:")
 
-# print "Bagels" if both variables are set to 0
+        if s == stringified_num:
+            print("You got it!")
+            break
 
-s = input("Enter your guess for the number here:")
+        guess_hints = []
 
-# validate user's guess?
+        for i in range(len(s)):
+            if stringified_num[i] == s[i]:
+                guess_hints.append('Fermi')
+            elif s[i] in stringified_num:
+                guess_hints.append('Pico')
 
-guess_hints = []
+        if len(guess_hints) == 0:
+            print("Bagels")
+        else:
+            feedback = ' '.join(guess_hints)
+            print(feedback)
 
-j = 0
-for i in range(len(s)):
-    if stringified_num[i] == s[i]:
-        guess_hints.append('Fermi')
-    elif s[i] in stringified_num:
-        guess_hints.append('Pico')
+        if num_guesses == GUESSES:
+            print("You used up all your guesses!")
+            break
 
-if len(guess_hints) == 0:
-    print("Bagels")
-else:
-    feedback = ' '.join(guess_hints)
-    print(feedback)
-
-# print(s)
+    still_playing = input("Type quit to quit or anything to play again!")
+    if still_playing == "quit":
+        break
